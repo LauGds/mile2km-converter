6b9fa6113f7b96272424446157b900e2ce26ebe0
LauGds <137927575+LauGds@users.noreply.github.com>
2023-07-24 10:00:36 -0700

    main.py

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ NUM = 1.609
 window = Tk()
 window.title("Mile to Km Converter")
 window.minsize(width=300, height=120)
-# window.config(padx=100, pady=200)
 
-# Label
 is_equal = Label(text="is equal to", font=("Arial", 15, "bold"))
 is_equal.grid(column=0, row=1)
-# is_equal.config(padx=50, pady=50)
 
 miles = Label(text="Miles", font=("Arial", 15, "bold"))
 miles.grid(column=2, row=0)
-# miles.config(padx=15, pady=15)
 
 km = Label(text="Km", font=("Arial", 15, "bold"))
 km.grid(column=2, row=1)
